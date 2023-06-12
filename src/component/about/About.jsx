f5b085b430bbccbe121a345cdd0c87b0c68dc2d7
delfina <delfinapazzavalia@gmail.com>
2023-06-12 18:38:24 -0300

    about component

---
+++ src/component/about/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../about/About.css"
+
+const About = () => {
+  return (
+    <>
+      <div className="about-me-container">
+        <h1 className="title">About Me</h1>
+        <p className="text">
+          Hello! I'm Delfina, <strong>{'<'}FullStack Web Developer{'>'}</strong> located
+          in Argentina. A year ago, I delved into this exciting world, and since
+          then, I haven't stopped learning and growing. I'm fascinated by
+          creating complete and functional solutions from scratch. I'm always
+          seeking new challenges and opportunities for personal and professional
+          development.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
