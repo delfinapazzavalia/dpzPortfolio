6dec285673412804e9e5d16454b9d85d2e907e57
“delfinapazzavalia” <delfinapazzavalia@gmail.com>
2023-12-21 15:47:19 -1000

    cvSection

---
+++ src/component/cv/Cv.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../cv/cv.ss";
+
+
+const Resume = () => {
+  return (
+    <>
+      <div id="Resume" className="resume">
+        <h2>RESUME</h2>
+        <div className="resume-cards">
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Resume;
