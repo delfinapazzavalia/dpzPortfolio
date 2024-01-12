cf80f8656d821d078c2c81f1b1c87d22cce0d2da
“delfinapazzavalia” <delfinapazzavalia@gmail.com>
2024-01-11 20:43:30 -1000

    cards

--- src/component/resume/cardsResume/CardRolling.jsx
+++ src/component/resume/cardsResume/CardRolling.jsx
@@ -10,7 +10,7 @@ const CardRolling = () => {
         <div className="card-content">
           <h3 className="card-title">Full Stack Web Developer</h3>
           <p className="card-description">Rolling Code School</p>
-          <p className="card-info">2022-2023</p>
+          <p className="card-info">2022 - 2023</p>
           <a className="card-link" target="_blank" rel="noopener noreferrer" href="https://certs.rollingcodeschool.com//ZGVsZmlwYXp6YXZhbGlhQGhvdG1haWwuY29t-1679573151976-1.png">
           View certificate
           </a>
